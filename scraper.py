673b8abe7afacdc245353afc0cd90baef3c8955a
GreciaNav <grecianav39@gmail.com>
2019-10-21 20:09:19 -0600

    Gets blue keywords from first Urban Dictionary result

--- scraper.py
+++ scraper.py
@@ -1,6 +1,21 @@
 from bs4 import BeautifulSoup
+import requests
+import re
 
-URL = "https://www.urbandictionary.com/define.php?term="
-term = input("Enter a term:")
+URL = 'https://www.urbandictionary.com/define.php?term='
+term = input('Enter a term: ')
 URL += term
 print(URL)
+
+html = requests.get(URL).text
+
+soup = BeautifulSoup(html, 'html.parser')
+definitions = soup.find('div', {'class': 'def-panel'})
+first_def = definitions.find('div', {'class': 'meaning'}).findAll('a')
+
+for w in first_def:
+    clean = re.compile('<.*?>')
+    word = re.sub(clean,'',w.text)
+    print(word)
+
+
