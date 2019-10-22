09ad6cb480e40d5af77c0914f8ba147425f3cf39
GreciaNav <grecianav39@gmail.com>
2019-10-21 20:30:03 -0600

    Gets blue keywords from first Urban Dictionary result (Deals with spaces)

--- scraper.py
+++ scraper.py
@@ -2,20 +2,23 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-URL = 'https://www.urbandictionary.com/define.php?term='
-term = input('Enter a term: ')
-URL += term
-print(URL)
+while(True):
+    URL = 'https://www.urbandictionary.com/define.php?term='
+    print('\n')
+    term = input('Enter a term: ')
+    URL += term
+    space = URL.replace(' ', '%20')
+    print(space)
 
-html = requests.get(URL).text
+    html = requests.get(URL).text
 
-soup = BeautifulSoup(html, 'html.parser')
-definitions = soup.find('div', {'class': 'def-panel'})
-first_def = definitions.find('div', {'class': 'meaning'}).findAll('a')
+    soup = BeautifulSoup(html, 'html.parser')
+    definitions = soup.find('div', {'class': 'def-panel'})
+    first_def = definitions.find('div', {'class': 'meaning'}).findAll('a')
 
-for w in first_def:
-    clean = re.compile('<.*?>')
-    word = re.sub(clean,'',w.text)
-    print(word)
+    for w in first_def:
+        clean = re.compile('<.*?>')
+        word = re.sub(clean,'',w.text)
+        print(word)
 
 
